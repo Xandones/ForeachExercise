b8d37c601c6fe9ec5f3a52351b577d4005035641
Alexandro Hugo Lopes <karkekoisaaeh@outlook.com>
2024-07-27 17:23:55 -0300

    Add project files.

---
+++ ForeachExercise.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float Grades[]{4.9f, 9.5f, 5.6f, 2.5f, 9.6f, 6.4f, 7.5f, 9.8f, 8.7f, 10.0f};
+	float GradesAverage = 0.0f;
+	int Size = sizeof(Grades) / sizeof(Grades[0]);
+
+	for (auto x : Grades)
+	{ 
+		GradesAverage += x;
+		cout << x << " ";
+	}
+	cout << "\n";
+	GradesAverage /= Size;
+	cout << "The average of the above grades is " << GradesAverage << "\n";
+}
